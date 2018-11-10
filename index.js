9c045e12ba54648a3eff13109548ff3f23628ba6
bestie <lucientown@outlook.com>
2018-11-10 11:26:55 +0800

    增加监控引入模版变化

--- index.js
+++ index.js
@@ -4,54 +4,51 @@ const loaderUtils = require('loader-utils');
 const nunjucks = require('nunjucks');
 const path = require('path');
 
-const njkLoader = (startPath, alias) => {
-  const resolvePath = filePath => {
-    if (typeof alias === 'object') {
-      for (key in alias) {
-        const patt = new RegExp(`^~${key}`);
-
-        if (patt.test(filePath)) {
-          return path.join(alias[key], filePath.replace(patt, ''));
+module.exports = function(source) {
+  const njkLoader = (startPath, alias) => {
+    const resolvePath = filePath => {
+      if (typeof alias === 'object') {
+        for (key in alias) {
+          const patt = new RegExp(`^~${key}`);
+  
+          if (patt.test(filePath)) {
+            return path.join(alias[key], filePath.replace(patt, ''));
+          }
         }
       }
-    }
-
-    return path.resolve(startPath, filePath);
+  
+      return path.resolve(startPath, filePath);
+    };
+  
+    return {
+      getSource: filePath => {
+        const completePath = resolvePath(filePath);
+        this.addDependency(completePath);
+        const dataBuffer = fs.readFileSync(completePath);
+        // const charset = chardet.detect(dataBuffer);
+  
+        return {
+          src: dataBuffer
+            .toString('UTF-8', 0, dataBuffer.length)
+            .replace(/({%.+?)>(.+?%})/gi, '$1&gt;$2')
+            .replace(/({%.+?)<(.+?%})/gi, '$1&lt;$2'), //将 > < 替换成实体字符,
+          path: completePath
+        };
+      }
+    };
   };
 
-  return {
-    getSource: filePath => {
-      const completePath = resolvePath(filePath);
-      const dataBuffer = fs.readFileSync(completePath);
-      const charset = chardet.detect(dataBuffer);
-
-      // console.log(dataBuffer.toString(charset, 0, dataBuffer.length))
-      // return
-      // console.log(completePath, charset)
-
-      return {
-        src: dataBuffer
-          .toString('UTF-8', 0, dataBuffer.length)
-          .replace(/({%.+?)>(.+?%})/gi, '$1&gt;$2')
-          .replace(/({%.+?)<(.+?%})/gi, '$1&lt;$2'), //将 > < 替换成实体字符,
-        path: completePath
-      };
-    }
-  };
-};
-
-module.exports = function loader(source) {
-  source = source
-    .replace(/({%.+?)>(.+?%})/gi, '$1&gt;$2')
-    .replace(/({%.+?)<(.+?%})/gi, '$1&lt;$2'); //将 > < 替换成实体字符
   const options = loaderUtils.getOptions(this) || {};
+  if (options.tags) {
+    source = source
+      .replace(/({%.+?)>(.+?%})/gi, '$1&gt;$2')
+      .replace(/({%.+?)<(.+?%})/gi, '$1&lt;$2'); //将 > < 替换成实体字符
+  }
   const env = new nunjucks.Environment(njkLoader(this.context, options.alias), {
     tags: options.tags
   });
   const compiled = nunjucks.compile(source, env);
-  // console.log(compiled.tmplStr.indexOf('head') >= 0 ? compiled.tmplStr : '')
   const rendered = compiled.render(options.context || {});
-  // const rendered = compiled.render(options.context || {}).replace(/\n|\r/g,'').replace(/"/g, '\\"');
 
   return rendered;
-};
+}
